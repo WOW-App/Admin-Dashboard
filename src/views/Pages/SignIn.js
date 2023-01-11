b04c828fb3ed92391a9724f70203470f352eb23d
WowDevBackend-1 <wowdeveloperbackend1@gmail.com>
2023-01-11 11:18:58 +0530

    admin signin through otp

--- src/views/Pages/SignIn.js
+++ src/views/Pages/SignIn.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React,{useState} from "react";
+import axios from "axios";
 // Chakra imports
 import {
   Box,
@@ -17,9 +18,99 @@ import {
 // Assets
 import signInImage from "assets/img/signInImage.png";
 import { FaApple, FaFacebook, FaGoogle } from "react-icons/fa";
+var phone=null;
+var mobile=null;
+var otp=null;
+var logID=null;
+var token=null;
+
+
+
+function phoneNo(e){
+  phone=e.target.value
+  mobile=phone
+
+}
+function otpField(e){
+  otp=e.target.value
+  console.log(otp)
+}
+
+function checkAdmin(){
+  var config = {
+    method: 'get',
+    url: 'https://development.wowapp.tech/dash/user',
+    headers: { 
+      'Authorization': token, 
+      'Content-Type': 'application/json', 
+      
+    }
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+}
+function otpLogin(phone){
+  
+  var data = JSON.stringify({
+    "mobile":mobile
+  });
+  
+  var config = {
+    method: 'post',
+    url: 'https://development.wowapp.tech/api/auth/otp/gen',
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    logID=(response.data.LogID)
+    console.log("log id is",logID)
+  })
+
+
+}
+
+function otpVerify(){
+  
+  console.log("otp is",logID,otp,typeof(mobile))
+  var data = JSON.stringify({
+    "LogID":  logID,
+    "otp":otp,
+    "phone": mobile
+  });
+  
+  var config = {
+    method: 'post',
+    url: 'https://development.wowapp.tech/api/auth/otp/verify',
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+  
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    token=response.data.token
+    if(response.data.otp_valid==true){
+      console.log("you are now member of wow")
+      checkAdmin()
+    }
+  })
+  
+}
 
 function SignIn() {
   // Chakra color mode
+
+  const [otp,setOtp]=useState(false);
   const textColor = useColorModeValue("gray.700", "white");
   const bgForm = useColorModeValue("white", "navy.800");
   const titleColor = useColorModeValue("gray.700", "blue.500");
@@ -59,14 +150,16 @@ function SignIn() {
               "unset"
             )}>
             <Text
-              fontSize='xl'
+              fontSize='x1'
               color={textColor}
               fontWeight='bold'
               textAlign='center'
-              mb='22px'>
-              Register With
+              mb='28px'>
+              Admin Portal
             </Text>
-            <HStack spacing='15px' justify='center' mb='22px'>
+            
+            
+            {/* <HStack spacing='15px' justify='center' mb='22px'>
               <Flex
                 justify='center'
                 align='center'
@@ -127,18 +220,20 @@ function SignIn() {
                   />
                 </Link>
               </Flex>
-            </HStack>
-            <Text
+            </HStack> */}
+            {/* <Text
               fontSize='lg'
               color='gray.400'
               fontWeight='bold'
               textAlign='center'
               mb='22px'>
               or
-            </Text>
+            </Text> */}
+            {otp==false && 
+            <div className="phone-number">
             <FormControl>
               <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
-                Name
+                Phone Number
               </FormLabel>
               <Input
                 variant='auth'
@@ -148,18 +243,35 @@ function SignIn() {
                 placeholder='Your full name'
                 mb='24px'
                 size='lg'
+                onChange={(e)=>{phoneNo(e)}}
               />
+              <Button
+                fontSize='10px'
+                variant='dark'
+                fontWeight='bold'
+                w='100%'
+                h='45'
+                mb='24px'
+                onClick={()=>{ if(phone!=null){phone=phone.split('')}else{phone=" "}; if(phone.length==10){setOtp(true) ; otpLogin(phone)}else{ console.log("enter valid phone number") ; phone=null}}}>
+                Send OTP
+              </Button>
+              </FormControl>
+            </div> }
+
+              {otp==true && <div className="otp-verify">
+              <FormControl>
               <FormLabel ms='4px' fontSize='sm' fontWeight='normal'>
-                Password
+                OTP
               </FormLabel>
               <Input
                 variant='auth'
                 fontSize='sm'
                 ms='4px'
                 type='password'
-                placeholder='Your password'
+                placeholder='your six digit otp'
                 mb='24px'
                 size='lg'
+                onChange={(e)=>{otpField(e)}}
               />
               <FormControl display='flex' alignItems='center' mb='24px'>
                 <Switch id='remember-login' colorScheme='blue' me='10px' />
@@ -173,8 +285,9 @@ function SignIn() {
                 fontWeight='bold'
                 w='100%'
                 h='45'
-                mb='24px'>
-                SIGN UP
+                mb='24px'
+                onClick={()=>{otpVerify()}}>
+                SIGN IN
               </Button>
             </FormControl>
             <Flex
@@ -184,17 +297,24 @@ function SignIn() {
               maxW='100%'
               mt='0px'>
               <Text color={textColor} fontWeight='medium'>
-                Already have an account?
+                
                 <Link
-                  color={titleColor}
+                  color='blue'
                   as='span'
                   ms='5px'
-                  href='#'
-                  fontWeight='bold'>
-                  Sign In
+                  href='http://www.google.com'
+                  fontWeight='lighter'
+                  textDecoration='underline'
+                  onClick={()=>{otpLogin(phone)}}>
+                  resend otp
                 </Link>
               </Text>
             </Flex>
+              </div> }
+            
+              
+             
+            
           </Flex>
         </Flex>
         <Box
