9f15ad01c8e74a45d14962e873a6403c59aef1f6
WowDevBackend-1 <wowdeveloperbackend1@gmail.com>
2023-02-02 11:43:32 +0530

    changes in signin

--- src/views/Pages/SignIn.js
+++ src/views/Pages/SignIn.js
@@ -50,9 +50,9 @@ function checkAdmin(){
   var config = {
     method: 'get',
     url: 'https://development.wowapp.tech/dash/user',
-    headers: { 
-      'Authorization': "Bearer "+token, 
-      'Content-Type': 'application/json', 
+    headers: {
+      'Authorization': "Bearer " +token,
+      'Content-Type': 'application/json',
       
     }
   };
@@ -117,12 +117,12 @@ async function otpVerify(){
   };
   
   await axios(config)
-  .then(async (response)=> {
+  .then(async (response) => {
     //console.log(JSON.stringify(response.data));
-    name=response.data.user.fullName;
-    token=(response.data.token);
-    
-    if(response.data.otp_valid==true){
+    name = response.data.user.fullName;
+    token =(response.data.token)
+  
+    if (response.data.otp_valid == true) {
       //aconsole.log("you are now member of wow")
       checkAdmin()
     }
@@ -139,8 +139,8 @@ function setLocalstorage(){
     localStorage.setItem('Token',null);
   
   }
-  if(!localStorage.getItem('Name')){
-    localStorage.setItem('Name',null);
+  if (!localStorage.getItem('Name')) {
+    localStorage.setItem('Name', null);
   
   }
   return;
@@ -163,162 +163,162 @@ function SignIn() {
   return (
     <>
     
-    {admin==false && 
-     <Flex position='relative' mb='40px'>
-     <Flex
-       minH={{ md: "1000px" }}
-       h={{ sm: "initial", md: "75vh", lg: "85vh" }}
-       w='100%'
-       maxW='1044px'
-       mx='auto'
-       justifyContent='space-between'
-       mb='30px'
-       pt={{ md: "0px" }}>
-       <Flex
-         w='100%'
-         h='100%'
-         alignItems='center'
-         justifyContent='center'
-         mb='60px'
-         mt={{ base: "50px", md: "20px" }}>
-         <Flex
-           zIndex='2'
-           direction='column'
-           w='445px'
-           background='transparent'
-           borderRadius='15px'
-           p='40px'
-           mx={{ base: "100px" }}
-           m={{ base: "20px", md: "auto" }}
-           bg={bgForm}
-           boxShadow={useColorModeValue(
-             "0px 5px 14px rgba(0, 0, 0, 0.05)",
-             "unset"
-           )}>
-           <Text
-           className="admin-portal"
-             fontSize='40px'
-             color={textColor}
-             fontWeight='bold'
-             textAlign='center'
-             mb='22px'>
-             Admin Panel
-           </Text>
-           
-           {otp==false && 
-           <div className="phone-number">
-           <FormControl>
-             <FormLabel ms='4px' fontSize='sm' fontWeight='normal' className="phone-no">
-               Phone Number
-             </FormLabel>
-             <Input
-               variant='auth'
-               fontSize='sm'
-               ms='4px'
-               type='text'
-               placeholder='Your Phone Number'
-               mb='24px'
-               size='lg'
-               onChange={(e)=>{phoneNo(e)}}
-             />
-             <Button
-               className='sent-otp'
-               fontSize='10px'
-               variant='dark'
-               fontWeight='bold'
-               w='100%'
-               h='45' 
-               mb='24px'
-               onClick={()=>{ if(phone!=null){phone=phone.split('')}else{phone=" "}; if(phone.length==10){setOtp(true) ; otpLogin(phone)}else{ console.log("enter valid phone number") ; phone=null}}}>
-               Send OTP
-             </Button>
-             </FormControl>
-           </div> }
+    {admin == false &&
+        <Flex position='relative' mb='40px'>
+          <Flex
+            minH={{ md: "1000px" }}
+            h={{ sm: "initial", md: "75vh", lg: "85vh" }}
+            w='100%'
+            maxW='1044px'
+            mx='auto'
+            justifyContent='space-between'
+            mb='30px'
+            pt={{ md: "0px" }}>
+            <Flex
+              w='100%'
+              h='100%'
+              alignItems='center'
+              justifyContent='center'
+              mb='60px'
+              mt={{ base: "50px", md: "20px" }}>
+              <Flex
+                zIndex='2'
+                direction='column'
+                w='445px'
+                background='transparent'
+                borderRadius='15px'
+                p='40px'
+                mx={{ base: "100px" }}
+                m={{ base: "20px", md: "auto" }}
+                bg={bgForm}
+                boxShadow={useColorModeValue(
+                  "0px 5px 14px rgba(0, 0, 0, 0.05)",
+                  "unset"
+                )}>
+                <Text
+                  className="admin-portal"
+                  fontSize='40px'
+                  color={textColor}
+                  fontWeight='bold'
+                  textAlign='center'
+                  mb='22px'>
+                  Admin Panel
+                </Text>
 
-             {otp==true && <div className="otp-verify">
-             <FormControl>
-             <FormLabel ms='4px' fontSize='sm' fontWeight='normal' className='otp'>
-               OTP
-             </FormLabel>
-             <Input
-               variant='auth'
-               fontSize='sm'
-               ms='4px'
-               type='password'
-               placeholder='your six digit otp'
-               mb='24px'
-               size='lg'
-               onChange={(e)=>{otpField(e)}}
-             />
-             <FormControl display='flex' alignItems='center' mb='24px'>
-               <Switch id='remember-login' colorScheme='blue' me='10px' />
-               <FormLabel htmlFor='remember-login' mb='0' fontWeight='normal' className='remember-me'>
-                 Remember me
-               </FormLabel>
-             </FormControl>
-             <Button
-               className='otp-signin'
-               fontSize='10px'
-               variant='dark'
-               fontWeight='bold'
-               w='100%'
-               h='45'
-               mb='24px'
-               onClick={async()=>{await otpVerify() ; if(check==true){setAdmin(true); console.log(token);localStorage.setItem('Admin',true);localStorage.setItem('Token',token);localStorage.setItem('Name',JSON.stringify(name)) }else{localStorage.setItem('Admin',false); console.log("You dont have access to this")} }}>
-               SIGN IN
-             </Button>
-           </FormControl>
-           <Flex
-             flexDirection='column'
-             justifyContent='center'
-             alignItems='center'
-             maxW='100%'
-             mt='0px'>
-             <Text color={textColor} fontWeight='medium'>
-               
-               <Link
-                 className="resend-otp"
-                 color='blue'
-                 as='span'
-                 ms='5px'
-                 href='#'
-                 fontWeight='lighter'
-                 textDecoration='underline'
-                 onClick={()=>{otpLogin(phone)}}>
-                 resend otp
-               </Link>
-             </Text>
-           </Flex>
-           
-             </div> }
-           
-             
-            
-           
-         </Flex>
-       </Flex>
-       <Box
-         overflowX='hidden'
-         h='100%'
-         w='100%'
-         left='0px'
-         position='absolute'
-         bgImage={signInImage}>
-         <Box
-           w='100%'
-           h='100%'
-           bgSize='cover'
-           bg='blue.500'
-           opacity='0.8'></Box>
-       </Box>
-     </Flex>
-   </Flex>}
+                {otp == false &&
+                  <div className="phone-number">
+                    <FormControl>
+                      <FormLabel ms='4px' fontSize='sm' fontWeight='normal' className="phone-no">
+                        Phone Number
+                      </FormLabel>
+                      <Input
+                        variant='auth'
+                        fontSize='sm'
+                        ms='4px'
+                        type='text'
+                        placeholder='Your Phone Number'
+                        mb='24px'
+                        size='lg'
+                        onChange={(e) => { phoneNo(e) }}
+                      />
+                      <Button
+                        className='sent-otp'
+                        fontSize='10px'
+                        variant='dark'
+                        fontWeight='bold'
+                        w='100%'
+                        h='45'
+                        mb='24px'
+                        onClick={() => { if (phone != null) { phone = phone.split('') } else { phone = " " }; if (phone.length == 10) { setOtp(true); otpLogin(phone) } else { console.log("enter valid phone number"); phone = null } }}>
+                        Send OTP
+                      </Button>
+                    </FormControl>
+                  </div>}
 
-   {admin==true && <><Dashboard name={name}/></>}
-   
+                {otp == true && <div className="otp-verify">
+                  <FormControl>
+                    <FormLabel ms='4px' fontSize='sm' fontWeight='normal' className='otp'>
+                      OTP
+                    </FormLabel>
+                    <Input
+                      variant='auth'
+                      fontSize='sm'
+                      ms='4px'
+                      type='password'
+                      placeholder='your six digit otp'
+                      mb='24px'
+                      size='lg'
+                      onChange={(e) => { otpField(e) }}
+                    />
+                    <FormControl display='flex' alignItems='center' mb='24px'>
+                      <Switch id='remember-login' colorScheme='blue' me='10px' />
+                      <FormLabel htmlFor='remember-login' mb='0' fontWeight='normal' className='remember-me'>
+                        Remember me
+                      </FormLabel>
+                    </FormControl>
+                    <Button
+                      className='otp-signin'
+                      fontSize='10px'
+                      variant='dark'
+                      fontWeight='bold'
+                      w='100%'
+                      h='45'
+                      mb='24px'
+                      onClick={async () => { await otpVerify(); if (check == true) { setAdmin(true); console.log(token); localStorage.setItem('Admin', true); localStorage.setItem('Token', token); localStorage.setItem('Name', JSON.stringify(name)) } else { localStorage.setItem('Admin', false); console.log("You dont have access to this"); alert("Only Admin account can access dashboard"); } }}>
+                      SIGN IN
+                    </Button>
+                  </FormControl>
+                  <Flex
+                    flexDirection='column'
+                    justifyContent='center'
+                    alignItems='center'
+                    maxW='100%'
+                    mt='0px'>
+                    <Text color={textColor} fontWeight='medium'>
+
+                      <Link
+                        className="resend-otp"
+                        color='blue'
+                        as='span'
+                        ms='5px'
+                        href='#'
+                        fontWeight='lighter'
+                        textDecoration='underline'
+                        onClick={() => { otpLogin(phone) }}>
+                        resend otp
+                      </Link>
+                    </Text>
+                  </Flex>
+
+                </div>}
 
 
-</>
+
+
+              </Flex>
+            </Flex>
+            <Box
+              overflowX='hidden'
+              h='100%'
+              w='100%'
+              left='0px'
+              position='absolute'
+              bgImage={signInImage}>
+              <Box
+                w='100%'
+                h='100%'
+                bgSize='cover'
+                bg='blue.500'
+                opacity='0.8'></Box>
+            </Box>
+          </Flex>
+        </Flex>}
+
+      {admin == true && <><Dashboard name={name} /></>}
+
+
+
+    </>
   );
 }
 
